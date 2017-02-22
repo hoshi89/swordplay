fba178f4e4953c1a9012037875efb885964f77e5
Robin Engström <robin.engstrom@op5.com>
2017-02-22 21:04:16 +0100

    WIP: Animation
    Signed-off-by: Robin Engström <robin.engstrom@op5.com>

---
+++ Assets/attackscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GetComponent<Animator> ().Play ("sword_attack");
+		} else {
+			GetComponent<Animator> ().Play ("Entry");
+		}
+	}
+}
